fe86ba12f6b4421644d9bd1d5ecf1770d1ba3084
sandreza <andrenogueirasouza@gmail.com>
2020-12-31 22:18:55 -0500

    some finishing touches

--- examples/visualize_mpistatearray.jl
+++ examples/visualize_mpistatearray.jl
@@ -6,68 +6,63 @@ using Makie, GLMakie, AbstractPlotting
 using ImageTransformations, Colors
 using AbstractPlotting.MakieLayout
 using GaussQuadrature, Random, Test
-include(pwd() * "/examples/permutation.jl")
-include(pwd() * "/examples/lagrange_interpolant.jl")
+using Statistics, Printf
+using ClimateMachine.MPIStateArrays
+include(pwd() * "/examples/gridhelper.jl")
 ##
 ClimateMachine.init()
 const ArrayType = ClimateMachine.array_type()
 const mpicomm = MPI.COMM_WORLD
 const FT = Float64
+# Domain
 Ω = Circle(-1,1) × Circle(-1,1) × Circle(-1,1)
-dims = ndims(Ω)
+# Discretization
 ex, ey, ez = (3,4,5) .* 10
 (npx, npy, npz) = (4,4,4)
 ClimateMachine.gpu_allowscalar(true)
-# Define Grid: might want to loop over element sizes and polynomial orders
 grid = DiscontinuousSpectralElementGrid(Ω, elements = (ex, ey, ez), polynomialorder = (npx, npy, npz), array = ArrayType)
-
+gridhelper = GridHelper(grid)
 x, y, z = coordinates(grid)
 xC, yC, zC = cellcenters(grid)
-
 ##
-# Check
-# (npx, npy, npz) = polynomialorders(grid)
+f = MPIStateArray{FT}(mpicomm, ArrayType, size(x)..., 1)
+f .=  1 .- (x .^2 + y .^2 + z .^2)
 
-#=
-@code_warntype checkgl(newx, rx)
-@code_warntype checkgl_2(newx, rx)
-@btime icheck = checkgl(newx, rx)
-@btime lagrange_eval(f, newx, newy, newz, rx, ry, rz, ωx, ωy, ωz)
-@btime lagrange_eval_nocheck(f, newx, newy, newz, rx, ry, rz, ωx, ωy, ωz)
-=#
+newsize = (10, 10, 10)
+nx, ny, nz = newsize
+newx = range(-1,1, length = nx)
+newy = range(-1,1, length = ny)
+newz = range(-1,1, length = nz)
+newf = zeros((nx,ny,nz))
+for i in 1:nx
+    for j in 1:ny
+        for k in 1:nz
+            location = (newx[i], newy[j], newz[k])
+            newf[i,j,k] = getvalue(f, location, gridhelper)
+        end
+    end
+end
+##
+scene, layout = layoutscene(resolution = (1920, 860))
+lscene = layout[2:4,2:4] = LScene(scene)
+layout[1, 2:4] = LText(scene, "MPI State Array", textsize = 50)
 
-## 
-# front load costs
-# quadrature points
-npx, npy, npz = polynomialorders(grid)
-x, y, z = coordinates(grid)
-ne = size(x)[2]
-rx, wx = GaussQuadrature.legendre(npx+1, both)
-ωx = baryweights(rx)
-ry, wy = GaussQuadrature.legendre(npy+1, both)
-ωy = baryweights(ry)
-rz, wz = GaussQuadrature.legendre(npz+1, both)
-ωz = baryweights(rz)
-# element getter
-xC, yC, zC = cellcenters(grid)
-ex = round(Int64, ne / sum(xC[1] .≈ xC))
-ey = round(Int64, ne / sum(yC[1] .≈ yC))
-ez = round(Int64, ne / sum(zC[1] .≈ zC))
-check = ne == ex * ey * ez
-check ? true : error("improper counting")
-p = getperm(xC, yC, zC, ex, ey, ez)
-lin = reshape(collect(1:length(xC)), (ex, ey, ez))
-linlocal = reshape(collect(1:(npx+1)*(npy+1)*(npz+1)), (npx+1, npy+1, npz+1))
+upperclim_slider = LSlider(scene, range = range(0, 1, length = 101), startvalue = 0.99)
+upperclim_node = upperclim_slider.value
+lowerclim_slider = LSlider(scene, range = range(0, 1, length = 101), startvalue = 0.01)
+lowerclim_node = lowerclim_slider.value
 
+clims = @lift((quantile(f[:], $lowerclim_node) , quantile(f[:], $upperclim_node)))
+cmap_rgb = to_colormap(:balance);
+volume!(lscene, -1..1, -1..1, -1..1, newf, colormap = cmap_rgb, colorrange = clims)
 
-# computation
+    lowerclim_string = @lift("lower clim quantile = " *  @sprintf("%0.2f", $lowerclim_node) * ", value = " * @sprintf("%0.1e", $clims[1]))
+    upperclim_string = @lift("upper clim quantile = " *  @sprintf("%0.2f", $upperclim_node) * ", value = " * @sprintf("%0.1e", $clims[2]))
+layout[1, 1] = vgrid!(
+    LText(scene, lowerclim_string, width = nothing),
+    lowerclim_slider,
+    LText(scene, upperclim_string, width = nothing),
+    upperclim_slider,
+)
+display(scene)
 
-
-# now test
-f = copy(z)
-fl = reshape(f, (npx+1, npy+1, npz+1, ex*ey*ez))
-location = (0.0, 0.0, -0.3)
-getvalue(fl, xC, yC, zC, location, p, lin, linlocal, x, y, z, rx, ry, rz, ωx, ωy, ωz)
-#=
-@btime getvalue(fl, xC, yC, zC, location, p, lin, linlocal, x, y, z, rx, ry, rz, ωx, ωy, ωz)
-=#
